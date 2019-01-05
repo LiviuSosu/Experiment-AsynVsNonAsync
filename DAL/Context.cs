9bec594a7c89ef36c90912f0ba7e7a3489f0d435
Liviu Şoşu <sosuliviu@gmail.com>
2019-01-06 00:36:45 +0200

    added the project

---
+++ DAL/Context.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DAL
+{
+    public class Context: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = EN614080\\SQLEXPRESS;Initial Catalog=Experiments_DB;Integrated Security= True"
+                ,x => x.MigrationsAssembly("DAL.Migrations")
+                );
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
