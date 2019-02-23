1cb2c5ac133dd2c6ca7c4f8bad371689f244f9ec
Liviu Şoşu <sosuliviu@gmail.com>
2019-02-23 07:00:46 +0200

    Cleaning the controllers

--- AsyncApi/Controllers/ValuesController.cs
+++ AsyncApi/Controllers/ValuesController.cs
@@ -15,37 +15,12 @@ namespace AsyncApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<string>>> Get()
         {
-            //Context context = new Context();
-            //var customers = await(from b in context.Customers
-            //                      orderby b.CustomerName
-            //                      select b).ToListAsync();
+            Context context = new Context();
+            var customers = await (from b in context.Customers
+                                   orderby b.CustomerName
+                                   select b).ToListAsync();
 
             return new string[] { "async 3.1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
